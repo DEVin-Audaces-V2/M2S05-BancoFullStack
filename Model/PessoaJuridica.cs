8425fa2763e175edd10b65ab3a09191a65445b19
Vitor Lassen <vitor@lassen.com.br>
2023-05-05 20:11:22 -0300

    FirstCommit

---
+++ Model/PessoaJuridica.cs
@@ -0,0 +1,25 @@
+
+
+namespace BancoFullStack.Model
+{
+    public class PessoaJuridica : Cliente
+    {
+        public string CNPJ { get; set; }
+        public string RazaoSocial { get; set; }
+        public string InscricaoEstadual { get; set; }
+
+        public PessoaJuridica()
+        {
+            
+        }
+
+        public PessoaJuridica(int numeroConta, string endereco, string telefone,
+                            string cnpj, string razaoSocial, string inscricaoEstadual) 
+                            :base (numeroConta, endereco,telefone)
+        {
+            CNPJ = cnpj;
+            RazaoSocial = razaoSocial;
+            InscricaoEstadual = inscricaoEstadual;
+        }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using BancoFullStack.Model;
+using BancoFullStack.Service;
+
+ClienteService clienteService = new ClienteService();
+
+clienteService.CriarConta();
+clienteService.CriarConta();
+
+clienteService.ExibirClientes();
+
+
+
+var pessoaFisica = new PessoaFisica(1,"Rua ", "123", "456", "Vitor", new DateTime(2005,05,5));
+
+Console.WriteLine(pessoaFisica.EhMaior());
+
+Console.ReadLine();
